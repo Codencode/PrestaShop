ce82a5c0bc3a0d274c54f30c157fb1c9cf3f370b
Codencode <info@codencode.it>
2025-03-07 15:31:38 +0100

    Aggiunta gestione delle pagine CMS

--- admin-dev/themes/new-theme/js/components/tinymce-editor.js
+++ admin-dev/themes/new-theme/js/components/tinymce-editor.js
@@ -24,7 +24,8 @@
  */
 import ComponentsMap from '@components/components-map';
 import {EventEmitter} from './event-emitter';
-
+/* // TODO <cnc-modifica> - TinyMCEEditor -  */
+import Router from '@components/router';
 const {$} = window;
 
 /**
@@ -37,6 +38,8 @@ class TinyMCEEditor {
   constructor(options) {
     const opts = options || {};
     this.tinyMCELoaded = false;
+    /* // TODO <cnc-modifica> - TinyMCEEditor::constructor() -  */
+    this.router = new Router();
     if (typeof opts.baseAdminUrl === 'undefined') {
       if (typeof window.baseAdminDir !== 'undefined') {
         opts.baseAdminUrl = window.baseAdminDir;
@@ -117,10 +120,21 @@ class TinyMCEEditor {
         this.changeToMaterial();
       },
       setup: (editor) => {
+        /* // TODO <cnc-modifica> - TinyMCEEditor::initTinyMCE() - */
+        editor.advantageLink = cfg.advantageLink;
+        /* ****************************************************** */
         this.setupEditor(editor);
       },
       ...config,
     };
+    /* // TODO <cnc-modifica> - TinyMCEEditor::initTinyMCE() - */
+    config.advantageLink = true;
+
+    if (config.advantageLink) {
+      cfg.advantageLink = true;
+      cfg.toolbar1 += ',advantageLink';
+    }
+    /* ****************************************************** */
 
     if (typeof window.defaultTinyMceConfig !== 'undefined') {
       Object.assign(cfg, window.defaultTinyMceConfig);
@@ -145,6 +159,58 @@ class TinyMCEEditor {
    * @param editor
    */
   setupEditor(editor) {
+    /* // TODO <cnc-modifica> - TinyMCEEditor::setupEditor() - Creazione pulsante */
+    if (editor.advantageLink) {
+      const allCmsRoute = this.router.generate('admin_cms_pages_all_cms');
+      editor.addButton('advantageLink', {
+        type: 'button',
+        text: '',
+        icon: 'link',
+        title: 'Internal link',
+        onclick () {
+          $.ajax({
+            type: 'POST',
+            url: allCmsRoute,
+            success(data) {
+              const values = [];
+              $.each(data, function (index, cms) {
+                values.push({ text: cms.meta_title, value: cms.id_cms})
+              })
+
+              editor.windowManager.open({
+                title: 'Aggiungi link interno',
+                body: [
+                  {
+                    type: 'textbox',
+                    name: 'text',
+                    label: 'Text to display',
+                    value: '',
+                  },
+                  {
+                    type: 'listbox',
+                    name: 'pageId',
+                    label: 'Page',
+                    values: values,
+                  }
+                ],
+                onsubmit: function (e) {
+                  let text = e.data.text;
+                  const pageId = e.data.pageId;
+
+                  if (!text) {
+                    text = 'NAME_CMS_ID=' + pageId
+                  }
+
+                  editor.insertContent('<a href="URL_CMS_ID=' + pageId + '">' + text + '</a>');
+                }
+              });
+            },
+            dataType: 'json',
+          });
+        }
+      });
+    }
+    /* ****************************************************** */
     editor.on('loadContent', (event) => {
       this.handleCounterTiny(event.target.id);
     });
