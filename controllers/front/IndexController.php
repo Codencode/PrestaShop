199ad15522082405264fa46ae3abf1ac7b2b0a02
Paolo Cunti @codencode <info@codencode.it>
2026-09-05 12:13:45 +0200

    temp

--- controllers/front/IndexController.php
+++ controllers/front/IndexController.php
@@ -20,6 +20,12 @@ class IndexControllerCore extends FrontController
             'HOOK_HOME' => Hook::exec('displayHome'),
         ]);
         $this->setTemplate('index');
+
+        //TODO <cnc-notice> ATTENZIONE: ho questo errore: The service "PrestaShopBundle\Entity\Repository\EmployeeRepository" has a dependency on a non-existent service "PrestaShop\PrestaShop\Core\Util\InternationalizedDomainNameConverter".
+        //$adminEmployeeContextProvider = $this->get(PrestaShop\PrestaShop\Adapter\Security\AdminEmployeeContextProvider\AdminEmployeeContextProvider);
+        $f = 0;
+        ////////////////////////////
+
     }
 
     /**
